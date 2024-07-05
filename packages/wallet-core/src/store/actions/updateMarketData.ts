b46dd76b338de7e51ba3b5ae4aa2fe7965e6ba4c
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2024-07-05 09:31:28 +0700

    Fix issue with fetching market data from providers

--- packages/wallet-core/src/store/actions/updateMarketData.ts
+++ packages/wallet-core/src/store/actions/updateMarketData.ts
@@ -3,19 +3,51 @@ import buildConfig from '../../build.config';
 import { getSwapProvider } from '../../factory/swap';
 import { MarketData, Network, SwapProviderType } from '../types';
 
+const MARKET_DATA_FETCH_TIMEOUT = 10000;
+
+interface ProviderTimeout {
+  info: {
+    [providerType in SwapProviderType]?: any;
+  };
+}
+
+let providerTimeout: ProviderTimeout = { info: {} }
+
+const withTimeout = (millis: number, provider: SwapProviderType, promise: Promise<MarketData[]>) => {
+  let timeoutData: MarketData[] = [];
+  const timeout: Promise<MarketData[]> = new Promise((resolve, _reject) =>
+    providerTimeout.info[provider] = setTimeout(
+          () => {
+            console.error('Timeout fetching market data for ', provider);
+            resolve(timeoutData)
+          },
+          millis));
+
+  return Promise.race([
+      promise,
+      timeout
+  ]);
+};
+
 export const updateMarketData = async (
   context: ActionContext,
   { network }: { network: Network }
 ): Promise<{ network: Network; marketData: MarketData[] }> => {
   const { commit } = rootActionContext(context);
   const supportedPairResponses = await Promise.allSettled(
-    Object.keys(buildConfig.swapProviders[network]).map((provider: SwapProviderType) => {
+    Object.keys(buildConfig.swapProviders[network]).map(async (provider: SwapProviderType) => {
       const swapProvider = getSwapProvider(network, provider);
-      return swapProvider.getSupportedPairs({ network }).then((pairs) => pairs.map((pair) => ({ ...pair, provider })));
+      const promise = swapProvider.getSupportedPairs({ network }).then((pairs) => pairs.map((pair) => ({ ...pair, provider })));
+      const result = await withTimeout(MARKET_DATA_FETCH_TIMEOUT, provider, promise)
+      console.log("TACA ===> provider = ", provider, ", result = ", result)
+      clearTimeout(providerTimeout.info[provider])
+      providerTimeout.info[provider] = null;
+      return result;
     })
   );
 
   let supportedPairs: MarketData[] = [];
+  console.log("TACA ===> supportedPairResponses = ", supportedPairResponses)
   supportedPairResponses.forEach((response) => {
     if (response.status === 'fulfilled') {
       supportedPairs = [...supportedPairs, ...response.value];
